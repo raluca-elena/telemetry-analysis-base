99119b07df61730d2c5531bfdcba441cd0b9edda
raluca-elena <ralucaelena1985@gmail.com>
2014-07-07 16:44:48 -0700

    removed decompress option

--- mapper.js
+++ mapper.js
@@ -1,8 +1,9 @@
 var yaml = require('js-yaml');
+var LineReadableStream = require('line-readable-stream');
 var fs   = require('fs');
 var child_process = require('child_process');
 
-var doc = yaml.safeLoad(fs.readFileSync('/etc/analysis-tools.yml', 'utf8'));
+var doc = yaml.safeLoad(fs.readFileSync('analysis-tools.yml', 'utf8'));
 var stdin = process.openStdin();
 var pr;
 
@@ -11,45 +12,19 @@ if (doc.language == 'binary') {
 } else if (doc.language == 'javascript') {
     pr = child_process.spawn('node', [doc.script]);
 } else if (doc.language == 'python') {
-    pr = child_process.spawn('/opt/analysis-tools/helper-functions/python-helper.py', ['mapper']);
+    pr = child_process.spawn('./helper-functions/python-helper.py', ['mapper']);
 } else {
     console.log("we don't support this language yet", doc.language);
     // TODO: crash in flames!
 }
 
-
 pr.on('exit', function(code){
     console.log("MAPPER exit code", code);
 })
 
+stdin.on('data', function (data) {
 
-if (doc.decompress) {
-    decompress();
-} else {
-    stdin.on('data', function (data) {
-        console.log("file sent to mapper", data.toString());
-        pr.stdin.write(data + "\n");
-    });
-    stdin.on("end", function() { pr.stdin.end(); })
-}
-
-function decompress() {
-    var decompress = child_process.spawn('./decompress.sh');
-
-    stdin.on('data', function (data) {
-        console.log("file sent to decompression is ", data.toString());
-        decompress.stdin.write(data + "\n");
-    });
-
-    decompress.stdout.on('data', function (data) {
-        pr.stdin.write(data);
-    });
-
-    stdin.on("end", function () {
-        decompress.stdin.end();
-    });
-
-    decompress.stdout.on("end", function () {
-        pr.stdin.end();
-    });
-}
+    console.log("file sent to mapper", data.toString());
+    pr.stdin.write(data + "\n");
+});
+stdin.on("end", function() { pr.stdin.end(); })
