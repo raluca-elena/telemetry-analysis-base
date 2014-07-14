fe8d4b6748c05526a68ed3e317f6af62a9eeadff
raluca-elena <ralucaelena1985@gmail.com>
2014-07-13 17:13:44 -0700

    resonable mapper

--- custom-mappers/mapper.py
+++ custom-mappers/mapper.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python
 import simplejson as json
 
-def mapper(uuid, js):
-    measures = []
-    for key, value in js.iteritems():
-        #print key
-        if key == 'histograms':
-            z = json.dumps(value)
-            q = json.loads(z)
-            #print 'ALOHA', q
-            for key, value in q.iteritems():
-                #print key, 1
-                measures.append(key + ' 1');
-                #print measures
-    #print "mea: ", measures
-    #print "str: ", str(measures)
-    return str(measures)
+def mapper(uuid, file, js):
+    return str(js['info']['OS'] + ' 1\n')
